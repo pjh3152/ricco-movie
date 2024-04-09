63b68270f60a1e3e2828b410e6f09b56d823f5f3
JaeHyung Park <pjh3152@gmail.com>
2024-04-09 14:43:17 +0900

    add ref in search input

--- src/views/TheSearch.vue
+++ src/views/TheSearch.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-12 text-center mb-4">
-        <input type="text" id="input" placeholder="영화제목을 입력해 주세요" autocomplete="off" v-model="keyword" @keyup.enter="search">
+        <input type="text" id="input" ref="input" placeholder="영화제목을 입력해 주세요" autocomplete="off" v-model="keyword" @keyup.enter="search">
       </div>
       <div class="col-2" v-for="d in movieList" :key="d.id">
         <MovieCard :data="d" />
@@ -16,6 +16,7 @@ import { ref, onMounted } from "vue";
 import { api } from "@/api/axios";
 import MovieCard from "@/components/MovieCard";
 
+const input = ref("");
 const keyword = ref("");
 const movieList = ref({});
 
@@ -24,7 +25,7 @@ onMounted(() => {
     keyword.value = localStorage.getItem("searchInput");
     search();
   }
-  document.getElementById("input").focus();
+  input.value.focus();
 })
 
 const search = async () => {
